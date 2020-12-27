a73f0f9efc99a80d64adad350b683b9dae9340ce
Storiwr <storiwr@users.noreply.github.com>
2020-12-27 17:45:43 -0500

    Initial Commit

---
+++ warchallenge/warchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warchallengeApp.swift
+//  warchallenge
+//
+//  Created by Stephen on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct warchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
