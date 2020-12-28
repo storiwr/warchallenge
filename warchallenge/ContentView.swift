8c5985674a0d6e57c52f2c82c734a08b5cb6b12f
Storiwr <storiwr@users.noreply.github.com>
2020-12-28 14:32:14 -0500

    initial commit

--- warchallenge/ContentView.swift
+++ warchallenge/ContentView.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                    .padding()
+                Spacer()
+                HStack {
+                    Image("card3")
+                        .padding()
+                    Image("card4")
+                        .padding()
+                }
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image("dealbutton").padding()
+                }
+                Spacer()
+                HStack {
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding()
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    } .padding()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding()
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }.padding()
+                }
+               Spacer()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .ignoresSafeArea()
     }
 }
